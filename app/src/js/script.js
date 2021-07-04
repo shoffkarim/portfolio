8401309707803c05cc7829b22cb3386d4ed291c0
shoffkarim <k4r1mch1k@gmail.com>
2021-07-04 17:15:57 +0500

    links and margins

--- app/src/js/script.js
+++ app/src/js/script.js
@@ -1,2 +1,12 @@
 // STYLES=============================================================================
 import '../sass/style.sass';
+
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+smoothLinks.forEach((item) => item.addEventListener('click', function (e) {
+  e.preventDefault();
+  const id = item.getAttribute('href');
+  document.querySelector(id).scrollIntoView({
+    behavior: 'smooth',
+    block: 'start'
+  });
+}));
